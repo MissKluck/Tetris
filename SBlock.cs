71812c1e2be0ab81f707e3f614ed3f6516cd8b03
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-14 17:23:39 +0100

    filled out the data for the sblock

--- SBlock.cs
+++ SBlock.cs
@@ -2,6 +2,25 @@ namespace Tetris
 {
     public class SBlock : Block
     {
+        //store the tile positions for the four rotation states
+        private readonly Position[][] tiles = new Position[][]
+        {
+            //State 0
+            new Position[] { new(0,1), new(0,2), new(1,0), new(1,1)},
+            //State 1
+            new Position[] { new(0,1), new(1,1), new(1,2), new(2,2)},
+            //State 2
+            new Position[] { new(1,1), new(1,2), new(2,0), new(2,1)},
+            //State 3
+            new Position[] { new(0,0), new(1,0), new(1,1), new(2,1)}
+        };
+        //fill out the required properties for the S-Block
+        //The ID should be 5
+        public override int Id => 5;
+        //the start offset should be 0 3
+        protected override Position StartOffset => new Position(0, 3);
 
+        //for the tiles property we return the tiles array above
+        protected override Position[][] Tiles => tiles;
     }
 }
