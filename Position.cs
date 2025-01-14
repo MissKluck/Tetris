e28fa9953cc648aa9b0fd47595ab3dafab5a46b1
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-14 13:15:54 +0100

    added comments

--- Position.cs
+++ Position.cs
@@ -2,9 +2,10 @@ namespace Tetris
 {
     public class Position
     {
+        //store the row and column
         public int Row { get; set; }
         public int Column { get; set; }
-
+        //simple constructor
         public Position(int row, int column)
         {
             Row = row;
